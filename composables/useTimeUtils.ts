bf5aeb4047e451b164a7f39ae3927a3250ab4816
@samanfattahi <samanft752@gmail.com>
2024-09-10 17:14:17 +0330

    init timeUtils

---
+++ composables/useTimeUtils.ts
@@ -0,0 +1,142 @@
+type convertToJalali = boolean | true
+type globalTimeUtilsOutput = Date | String
+
+export function useTimeUtils(convertState?: convertToJalali) {
+
+    function customFullDateConvert(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            convertFullDateToJalali(date);
+        }
+        return convertFullDateToGregorian(date);
+    }
+
+    function customConvertYear(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            return convertYearToJalali(date);
+        }
+        return convertYearToGregorian(date);
+    }
+
+    function customConvertMonth(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            return convertMonthToJalali(date);
+        }
+        return convertMonthToGregorian(date);
+    }
+
+    function customConvertHourAndMinute(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            return convertHourAndMinute(date);
+        }
+        return convertHourAndMinuteToGregorian(date);
+    }
+
+    function customConvertWeekDay(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            return convertWeekdayToJalali(date);
+        }
+        return convertWeekdayToGregorian(date);
+    }
+
+    function customConvertDate(date: Date): globalTimeUtilsOutput {
+        if (convertState) {
+            return convertDayToJalali(date);
+        }
+        return convertDayToGregorian(date);
+    }
+
+    function convertFullDateToJalali(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("fa-IR");
+    }
+
+    function convertFullDateToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("en-GB");
+    }
+
+    function convertYearToJalali(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("fa-IR", {
+            year: "numeric",
+        });
+    }
+
+    function convertYearToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("en-GB", {
+            year: "numeric",
+        });
+    }
+
+    function convertWeekdayToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("en-US", {
+            weekday: "long",
+        });
+    }
+
+    function convertWeekdayToJalali(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("fa-IR", {
+            weekday: "long",
+        });
+    }
+
+    function convertDayToJalali(date: Date): globalTimeUtilsOutput {
+        return new Date(date)
+            .toLocaleDateString("fa-IR", {
+                day: "numeric",
+            })
+            .replace("م", "");
+    }
+
+    function convertMonthToJalali(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("fa-IR", {
+            month: "long",
+        });
+    }
+
+    function convertDayToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("en-US", {
+            day: "numeric",
+        });
+    }
+
+    function convertMonthToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleDateString("en-US", {
+            month: "long",
+        });
+    }
+
+    function convertHourAndMinute(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleTimeString("fa-IR", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    }
+
+    function convertHourAndMinuteToGregorian(date: Date): globalTimeUtilsOutput {
+        return new Date(date).toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    }
+
+    return {
+        customFullDateConvert,
+        customConvertYear,
+        customConvertMonth,
+        customConvertHourAndMinute,
+        customConvertWeekDay,
+        customConvertDate,
+        convertFullDateToJalali,
+        convertFullDateToGregorian,
+        convertYearToJalali,
+        convertYearToGregorian,
+        convertWeekdayToGregorian,
+        convertWeekdayToJalali,
+        convertDayToJalali,
+        convertMonthToJalali,
+        convertDayToGregorian,
+        convertMonthToGregorian,
+        convertHourAndMinute,
+        convertHourAndMinuteToGregorian
+
+    }
+}
+
